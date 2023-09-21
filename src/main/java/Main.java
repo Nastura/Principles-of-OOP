9c32d3c54da79726de930ac725e06db93c46fa18
Nastura <chulmost@gmail.com>
2023-09-21 13:13:45 +0300

    update file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,31 @@
-package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Post post = new Post();
+
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1991;
+
+
+        post.name = "Тимур";
+        post.seriesPassport = "4518";
+        post.numberPassport = "454465";
+        post.patronymic = "Маратович";
+        post.phone = "+7(999)-555-94-99";
+        post.surname = "Руфатов";
+        post.subscription = true;
+
+        // заполните другие поля
+
+        System.out.println(post.birthday.day);
+
+        System.out.println(post.name);
+        System.out.println(post.patronymic);
+        System.out.println(post.phone);
+        System.out.println(post.surname);
+
+
     }
 }
---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+
+    FormDate birthday = new FormDate();
+    public String name;         // Поле для имени
+    public String seriesPassport;         // Поле для серии паспорта
+    public String numberPassport;     // Поле для номера паспорта
+    public String patronymic;   // Поле для Отчества
+    public String phone;          // Поле для Номера телефона
+    public String surname;      // Поле для Фамилии
+    public boolean subscription; // Поле для
+
+
+
+}
